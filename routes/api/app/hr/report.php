69ccfd9bebc1d4e348058c22a129afa5bbfcdb3e
waheedsafi <wahidsafi.198@gmail.com>
2025-05-14 14:36:21 +0430

    reporttypeenum

---
+++ routes/api/app/hr/report.php
@@ -0,0 +1,10 @@
+
+<?php
+
+use App\Http\Controllers\api\app\hr\report\ReportController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/hr/report/types', [ReportController::class, 'reportTypes']);
+Route::prefix('v1')->middleware(["authorized:" . 'user:api'])->group(function () {});
