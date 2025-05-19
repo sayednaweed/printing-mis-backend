68b5ebda818a5c22423e5dde1ac2b308e5483c90
waheedsafi <wahidsafi.198@gmail.com>
2025-05-19 16:04:58 +0430

    attendace

--- app/Models/AttendanceStatus.php
+++ app/Models/AttendanceStatus.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceStatus extends Model
 {
     //
+    protected $guarded = [];
 }
--- app/Models/AttendanceStatusTran.php
+++ app/Models/AttendanceStatusTran.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceStatusTran extends Model
 {
     //
+    protected $guarded = [];
 }
