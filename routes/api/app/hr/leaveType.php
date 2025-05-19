9e6d8e98397d0bd6e4e68f10cf0d6a7cfb06d68f
waheedsafi <wahidsafi.198@gmail.com>
2025-05-19 10:52:45 +0430

    leave type

--- routes/api/app/hr/leaveType.php
+++ routes/api/app/hr/leaveType.php
@@ -6,4 +6,7 @@ use App\Http\Controllers\api\app\hr\attendance\LeaveTypeController;
 
 Route::prefix('v1')->middleware(["authorized:" . 'user:api'])->group(function () {
     Route::get('/leave-types', [LeaveTypeController::class, 'index']);
+    Route::post('/leave-types', [LeaveTypeController::class, 'store']);
+    Route::put('/leave-types', [LeaveTypeController::class, 'update']);
+    Route::get('/leave-types/{id}', [LeaveTypeController::class, 'edit']);
 });
