9a659409c283aefe88087ad13438c23eab4ca5a1
waheedsafi <wahidsafi.198@gmail.com>
2025-05-22 10:21:11 +0430

    shift

--- routes/api/app/hr/shift.php
+++ routes/api/app/hr/shift.php
@@ -5,15 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\app\hr\shift\ShiftController;
 
 Route::prefix('v1')->middleware(["authorized:" . 'user:api'])->group(function () {
-    Route::post('/shift/store', [ShiftController::class, "store"]);
-    Route::get('/shifts', [ShiftController::class, "shifts"]);
-    Route::get('/shift/{id}', [ShiftController::class, "shift"]);
-    Route::put('/shiftupdate/{id}', [ShiftController::class, 'update']);
-    Route::get('/shift/{id}/edit', [ShiftController::class, 'edit']);
-
+    Route::post('/shift', [ShiftController::class, "store"]);
+    Route::get('/shift', [ShiftController::class, "shifts"]);
+    Route::get('/shift/{id}', [ShiftController::class, "edit"]);
+    Route::put('/shift/{id}', [ShiftController::class, 'update']);
 });
-
-
-
-
-
