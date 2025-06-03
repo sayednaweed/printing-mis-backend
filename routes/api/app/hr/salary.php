87de52f4885528b9ec81bdd2937f2bd6ceb72635
sayednaweedsayedy <sayednaweedsayedy@gmail.com>
2025-06-03 09:07:57 +0430

    salary permission added

--- routes/api/app/hr/salary.php
+++ routes/api/app/hr/salary.php
@@ -6,9 +6,9 @@ use App\Enums\Permission\HrPermissionEnum;
 use App\Http\Controllers\api\app\hr\salary\SalaryController;
 
 Route::prefix('v1')->middleware(["authorized:" . 'user:api'])->group(function () {
-    Route::get('/salaries', [SalaryController::class, 'index'])->middleware(["HasMainPermission:" . HrPermissionEnum::attendance->value . ',' . 'view']);
-    Route::get('/salaries/{id}', [SalaryController::class, 'show'])->middleware(["HasMainPermission:" . HrPermissionEnum::attendance->value . ',' . 'view']);
-    Route::post('/salaries', [SalaryController::class, 'store'])->middleware(["HasMainPermission:" . HrPermissionEnum::attendance->value . ',' . 'add']);
-    Route::get('/salaries-show', [SalaryController::class, 'showAttendance'])->middleware(["HasMainPermission:" . HrPermissionEnum::attendance->value . ',' . 'view']);
+    Route::get('/salaries', [SalaryController::class, 'index'])->middleware(["HasMainPermission:" . HrPermissionEnum::salaries->value . ',' . 'view']);
+    Route::get('/salaries/{id}', [SalaryController::class, 'show'])->middleware(["HasMainPermission:" . HrPermissionEnum::salaries->value . ',' . 'view']);
+    Route::post('/salaries', [SalaryController::class, 'store'])->middleware(["HasMainPermission:" . HrPermissionEnum::salaries->value . ',' . 'add']);
+    Route::get('/salaries-show', [SalaryController::class, 'showAttendance'])->middleware(["HasMainPermission:" . HrPermissionEnum::salaries->value . ',' . 'view']);
     Route::get('/salaries/statuses', [SalaryController::class, 'statuses']);
 });
