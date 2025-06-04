f831af5fac83c7a4399386cd0703712786079127
waheedsafi <wahidsafi.198@gmail.com>
2025-06-04 10:05:00 +0430

    account store

--- app/Http/Controllers/api/app/hr/account/AccountController.php
+++ app/Http/Controllers/api/app/hr/account/AccountController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\api\app\hr\account;
 
+use App\Models\Account;
+use App\Enums\LanguageEnum;
+use App\Models\AccountTran;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\app\account\AccountStoreRequest;
+use App\Models\AccountBalance;
 
 class AccountController extends Controller
 {
@@ -37,5 +42,86 @@ class AccountController extends Controller
             [],
             JSON_UNESCAPED_UNICODE
         );
+    }
+
+    public function store(AccountStoreRequest $request)
+    {
+
+        $request->validate();
+        $locale = App::getLocale();
+        DB::transaction();
+
+        $authUser = $request->user();
+        $account =    Account::create([
+            'code' => $request->code,
+            'detail' => $request->detail,
+            'user_id' => $authUser->id
+        ]);
+
+        foreach (LanguageEnum::LANGUAGES as $code => $name) {
+
+            AccountTran::create([
+                "value" => $request["name_${name}"],
+                "language_name" => $code,
+                "account_id" => $account->id
+            ]);
+        }
+        foreach ($request->account_balance as $key => $account) {
+            AccountBalance::create(
+                [
+                    'account_id'  => $account->id,
+                    'currency_id' => $account->currency_id,
+                    'balance' => 0.0
+                ]
+            );
+        }
+
+        $name = $request['name_english'];
+        if ($locale === 'fa') {
+            $name = $request['name_farsi'];
+        }
+        if ($locale === 'ps') {
+            $name = $request['name_pashto'];
+        }
+
+        $data = [
+            'id' => $account->id,
+            'code' => $request->code,
+            'username' => $authUser->username,
+            'name' => $name,
+            'created_at' => $account->created_at,
+            'detail' => $account->detail
+
+        ];
+
+        DB::commit();
+        return response()->json([
+            'message' => __('app_translation.success'),
+            'account' => $data,
+        ]);
+    }
+
+    public function edit($id)
+    {
+
+        DB::table('accounts as ac')
+            ->where('ac.id', $id)
+            ->leftJoin('account_trans as act', 'ac.id', '=', 'act.account_id')
+            ->select(
+                'ac.id',
+                'ac.code',
+                'ac.detail',
+                'ac.created_at',
+                DB::raw("MAX(CASE WHEN act.language_name = 'fa' THEN value END) as farsi"),
+                DB::raw("MAX(CASE WHEN act.language_name = 'en' THEN value END) as english"),
+                DB::raw("MAX(CASE WHEN act.language_name = 'ps' THEN value END) as pashto")
+            )
+            ->groupBy('ac.id')
+            ->first();
+        DB::table('account_balances as acb')
+            ->join('currencies as cur', function ($join) use ($locale) {
+                $join->on('acb.currency_id', '=', 'cur.id')
+                    ->where('langauge_name', $locale);
+            });
     }
 }
